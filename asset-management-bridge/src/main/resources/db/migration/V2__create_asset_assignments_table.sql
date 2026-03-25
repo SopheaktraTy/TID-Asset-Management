90ff96b8fcc3b5e9405660eea51b7854a78d5da4
Ty Sopheaktra <sopheaktra.t@bakertilly.com.kh>
2026-03-25 17:53:42 +0700

    refactor: add the some column in the table assignment

--- asset-management-bridge/src/main/resources/db/migration/V2__create_asset_assignments_table.sql
+++ asset-management-bridge/src/main/resources/db/migration/V2__create_asset_assignments_table.sql
@@ -5,9 +5,12 @@ CREATE TABLE IF NOT EXISTS asset_assignments (
     department VARCHAR(100),
     job_title VARCHAR(100),
     assigned_by VARCHAR(100),
+    assigned_by_user_id BIGINT REFERENCES users(id),
     assigned_date DATE NOT NULL DEFAULT CURRENT_DATE,
     returned_date DATE,
     return_condition VARCHAR(100),
+    confirm_return_by VARCHAR(100),
+    confirm_return_by_user_id BIGINT REFERENCES users(id),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
