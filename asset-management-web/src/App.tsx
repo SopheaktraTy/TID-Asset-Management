a17d9fe2b534f4eef78ed9e51d29e7ccdc9f2cc6
Ty Sopheaktra <sopheaktra.t@bakertilly.com.kh>
2026-03-27 11:08:36 +0700

    chore: Initialize new web project with React, TypeScript, Vite, Tailwind CSS, Tanstack Query, Zustand, and Axios.

---
+++ asset-management-web/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
